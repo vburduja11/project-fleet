e294728c5404c36c789bd2c94bf21e2140075629
vburduja11 <vburduja11@gmail.com>
2023-03-06 13:39:03 -0600

    US4_Vadim is finished

--- src/test/java/com/fleet/step_definitions/US4_StepDefs.java
+++ src/test/java/com/fleet/step_definitions/US4_StepDefs.java
@@ -1,30 +1,43 @@
 package com.fleet.step_definitions;
 
 import com.fleet.pages.DashboardPage;
+import com.fleet.utilities.BrowserUtils;
+import com.fleet.utilities.Driver;
+import io.cucumber.java.bs.A;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
 
 public class US4_StepDefs {
-    @Given("the user click the vehicle contracts under the fleet")
-    public void the_user_click_the_vehicle_contracts_under_the_fleet() {
-        new DashboardPage().waitUntilLoaderScreenDisappear();
-        new DashboardPage().navigateToModule("Fleet","Vehicle Contracts");
+
+    DashboardPage dashboardPage = new DashboardPage();
+    @And("the user navigate to {string} - {string}")
+    public void theUserNavigateTo(String tab, String module) {
+
+        dashboardPage.waitUntilLoaderScreenDisappear();
+        dashboardPage.navigateToModule(tab,module);
+    }
+
+
+    @Then("the title contains {string}")
+    public void theTitleContains(String expectedTitle) {
+        dashboardPage.waitUntilLoaderScreenDisappear();
+      Assert.assertEquals(expectedTitle,Driver.getDriver().getTitle());
 
     }
 
 
     @Then("user should see an error message {string}")
     public void userShouldSeeAnErrorMessage(String expected) {
-        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.waitUntilLoaderScreenDisappear();
         Assert.assertTrue(expected,dashboardPage.flashMessage.isDisplayed());
 
 
     }
 
-    @And("the user navigate to {string} - {string}")
-    public void theUserNavigateTo(String tab, String module) {
-        DashboardPage dashboardPage = new DashboardPage();
+
+    @Then("the page url is {string}")
+    public void thePageUrlIs(String expectedUrl) {
         dashboardPage.waitUntilLoaderScreenDisappear();
-        dashboardPage.navigateToModule(tab,module);
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedUrl));
     }
 }
--- src/test/resources/features/US4.feature
+++ src/test/resources/features/US4.feature
@@ -4,11 +4,19 @@ Feature: Vehicle contracts
     Given the user is on the login page
 
 
-  Scenario: Verify user can access the Vehicle contracts page
-    Given the user logged in as "sales manager"
-    And the user navigate to "Fleet" - "Vehicle Contracts"
+  Scenario Outline: US4AC1TC1 Verify user can access the Vehicle contracts page
+    Given the user logged in as "<userType>"
+    And the user navigate to "<tab>" - "<module>"
+    Then the title contains "<title>"
+    Then the page url is "<url>"
 
-  Scenario: Verify user can not access the Vehicle contracts page
+    Examples:
+      | userType      | tab   | module            | title                                                                | url                                                           |
+      | sales manager | Fleet | Vehicle Contracts | All - Vehicle Contract - Entities - System - Car - Entities - System | https://qa.intabella.com/entity/Extend_Entity_VehicleContract |
+      | store manager | Fleet | Vehicle Contracts | All - Vehicle Contract - Entities - System - Car - Entities - System | https://qa.intabella.com/entity/Extend_Entity_VehicleContract |
+
+
+  Scenario: US4AC2TC2 Verify user can not access the Vehicle contracts page
     Given the user logged in as "driver"
     And the user navigate to "Fleet" - "Vehicle Contracts"
     Then user should see an error message "You do not have permission to perform this action."
