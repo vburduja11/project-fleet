22884de9979fad30b5047920e1cdb15dfb3acc0c
Matvey_Cydeo <m6931357@gmail.com>
2023-03-08 10:47:04 -0500

    In Progress US09

--- src/test/java/com/fleet/runners/FailedTestRunner.java
+++ src/test/java/com/fleet/runners/FailedTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "@target/rerun.txt",
-        glue = "com/project_name/step_definitions"
+        glue = "com/fleet/step_definitions"
 )
 public class FailedTestRunner {
 }
